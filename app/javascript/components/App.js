70c73d0a314efec337ac0010e44ea80f2e9ba1de
vvoo21 <vaneolipa@gmail.com>
2023-08-19 20:57:13 -0500

    Update App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Greeting from './Greeting'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <Routes>
+            <Route path='/' element={<Greeting/>}/>
+        </Routes>
+    </div>
   )
 }
 
