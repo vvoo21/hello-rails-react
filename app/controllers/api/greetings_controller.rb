917fc0298ff4bb7e1f9ace0096cc694058718fb3
vvoo21 <vaneolipa@gmail.com>
2023-08-19 20:47:28 -0500

    Add index method

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,2 +1,8 @@
 class Api::GreetingsController < ApplicationController
+    def index
+        @greetings = Message.all
+        rand_ind = rand(@greetings.size)
+        @rand_greeting = @greetings[rand_ind]
+        render json: @rand_greeting
+    end
 end
